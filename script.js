2c7bbb79c15850542692226244cc6666c4547435
chrishenaku <88736752+chrishenaku@users.noreply.github.com>
2021-09-05 21:10:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var names=new array();
+names[0]="yaakov";
+names[1]="chris";
+names[2]="albert";
+names[3]="wendy";
+names[4]="Reginald";
+names[5]="jake";
+names[6]="christian";
+names[7]="eric";
+names[8]="Nancy";
+names[9]="ivan";
+
+for (var i = 0; i <names.length; i++) {
+	console.log(names[i]);
+};
